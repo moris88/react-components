8366a3e4438936667899a1a0754fab074dc48551
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-06-17 19:00:27 +0200

    :bug: fix

--- Test.jsx
+++ Test.jsx
@@ -1,7 +1,6 @@
 function Test(props) {
   const { data } = props
-  if (data) return <pre>{JSON.stringify(data, null, 5)}</pre>
-  else <></>
+  return <pre>{JSON.stringify(data, null, 5)}</pre>
 }
 
 export default Test
--- Test.tsx
+++ Test.tsx
@@ -4,8 +4,7 @@ interface TestProps {
 
 function Test(props: TestProps) {
   const { data } = props
-  if (data) return <pre>{JSON.stringify(data, null, 5)}</pre>
-  else <></>
+  return <pre>{JSON.stringify(data, null, 5)}</pre>
 }
 
 export default Test
